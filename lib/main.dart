11e4f6fdd5f4f591bb5610ed03f8c057e173dfa9
Rrissoli <raphels123@gmail.com>
2023-03-27 12:25:53 -0300

    oo

--- lib/main.dart
+++ lib/main.dart
@@ -14,84 +14,13 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Container(
-
-          color: Colors.black,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.cyan,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.pinkAccent,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.purple,
-                    width: 50,
-                    height: 50,
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.cyan,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.pinkAccent,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.purple,
-                    width: 50,
-                    height: 50,
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.cyan,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.pinkAccent,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.purple,
-                    width: 50,
-                    height: 50,
-                  )
-                ],
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    print("você apertou o botão");
-                  },
-                  child: Text('Aperte o botão!')
-              )
-            ],
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Firt pass'),
           ),
-        ));
+          body: Container(),
+          floatingActionButton: FloatingActionButton(onPressed: (){},),
+        ),
+    );
   }
 }
---
+++ lib/screen_one.dart
@@ -0,0 +1,13 @@
+// Container(
+// color: Colors.white,
+// child: Column(
+// mainAxisAlignment: MainAxisAlignment.start,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// children: [
+// AppBar(backgroundColor: Colors.blue,
+// title: Title(color: Colors.white, child: Text('Tarefas')),
+// ),
+//
+// ],
+// ),
+// )
